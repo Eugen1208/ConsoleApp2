60b41c4eea0caca2473816dc7c3ebdd600ba5936
User <User@E-NOTE>
2022-10-22 19:04:27 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Квадраты чисел от 1 до {0}:", n);
+            for (uint i = 1, sqr = 0; i <= n; i++)
+                Console.WriteLine("{0}^2 = {1}", i, sqr += i * 2 - 1);
+            Console.ReadKey();
+        }
+    }
+}
